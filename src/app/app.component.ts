55c631e49baa56b16d51e3cceab2d15a39016375
IEVN1003-20001612 <20001612@alumnos.utleon.edu.mx>
2024-10-12 11:04:37 -0600

    zodiaco

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Ejemplo1Component } from './formularios/ejemplo1/ejemplo1.component';
-import { ZodiacComponent } from './formularios/zodiaco/zodiaco.component';
+import { ZodiacoComponent } from './formularios/zodiaco/zodiaco.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Ejemplo1Component, ZodiacComponent],
+  imports: [RouterOutlet, Ejemplo1Component, ZodiacoComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
